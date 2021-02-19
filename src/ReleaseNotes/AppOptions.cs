528754d0337d850813401777375e9ed2cb4e7f0a
fazzani <heni.fazzani@gmail.com>
2021-02-19 23:08:20 +0100

    clean code

--- src/ReleaseNotes/AppOptions.cs
+++ src/ReleaseNotes/AppOptions.cs
@@ -3,7 +3,6 @@
     public class AppOptions
     {
         public const string SectionName = "App";
-        public string NoteTpl { get; set; }
         public string Query { get; set; }
     }
 }
